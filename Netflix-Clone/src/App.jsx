257eb90551b086d4fd74a327dc222bd4765aecd8
sofonyasrb <92786649+sofonyasrb@users.noreply.github.com>
2024-09-08 20:28:49 -0600

    folder structure commit

---
+++ Netflix-Clone/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <p>Test Page</p>
+    </>
+  )
+}
+
+export default App
---
+++ test/empty.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const empty = () => {
+  return (
+    <div>
+      <p>New netfilx project</p>
+    </div>
+  )
+}
+export default empty
